caada218c9c3fe8f2059d464578d1043c3fd2cd0
Al-Amin Talukdar <mail@timam.io>
2024-01-21 00:55:40 +0600

    Refactor directory validation in update-values-tag.sh
    The directory validation mechanism in update-values-tag.sh script has been optimized. Removed the unnecessary command `ls -larth` and redundant directory change, instead merged the validations to directly check the existence of the specific microservice environment directory.

--- update-deployment/update-values-tag.sh
+++ update-deployment/update-values-tag.sh
@@ -2,19 +2,17 @@
 
 set -e
 
-ls -larth
-
 # Let's test a few assumptions from our inputs on the deployment repo we just checked out
 if [ ! -d "${REPO_WORK_PATH}/${MICROSERVICE_NAME}" ]; then
     echo "Repo ${REPO_WORK_PATH}/${MICROSERVICE_NAME} does not exist. Exiting."
     exit 1
 fi
 
-cd "${REPO_WORK_PATH}/${REPOSITORY_NAME}"
-if [ ! -d "${ENVIRONMENT}" ]; then
-    echo "${ENVIRONMENT} does not exist in deployment repo or not a valid env spec. Exiting."
+if [ ! -d "${REPO_WORK_PATH}/${MICROSERVICE_NAME}/${ENVIRONMENT}" ]; then
+    echo "${REPO_WORK_PATH}/${MICROSERVICE_NAME}/${ENVIRONMENT} does not exist. Exiting."
     exit 1
 fi
+
 
 
 
