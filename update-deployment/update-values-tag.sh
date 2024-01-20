0950efceb171866c9b8947f7bde7355d864a8eee
Al-Amin Talukdar <mail@timam.io>
2024-01-21 01:21:44 +0600

    Refactor and optimize update-values-tag.sh script
    The update-values-tag.sh script has been improved and optimized by revamping the directory validation process. It now checks for the existence of the microservice's environment directory directly, instead of separately validating the microservice and environment directories. Additionally, the unnecessary listing and directory change commands have been removed for a more streamlined process.

--- update-deployment/update-values-tag.sh
+++ update-deployment/update-values-tag.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
-
 set -e
 
-# Let's test a few assumptions from our inputs on the deployment repo we just checked out
-if [ ! -d "${REPO_WORK_PATH}/${MICROSERVICE_NAME}" ]; then
-    echo "Repo ${REPO_WORK_PATH}/${MICROSERVICE_NAME} does not exist. Exiting."
+# The directories to check
+repo="${REPO_WORK_PATH}/${MICROSERVICE_NAME}"
+
+environment_dir="${repo}/${ENVIRONMENT}"
+
+# Check if the directories exist
+if [ ! -d "$repo" ] || [ ! -d "$environment_dir" ]; then
+    echo "Either $repo or $environment_dir does not exist. Exiting."
     exit 1
 fi
 
-if [ ! -d "${REPO_WORK_PATH}/${MICROSERVICE_NAME}/${ENVIRONMENT}" ]; then
-    echo "${REPO_WORK_PATH}/${MICROSERVICE_NAME}/${ENVIRONMENT} does not exist. Exiting."
+# Change to the environment directory
+cd "$environment_dir"
+
+# The file we want to update
+yaml_file="values_tag.yaml"
+
+# Make sure the file exists
+if [ ! -f "$yaml_file" ]; then
+    echo "$yaml_file does not exist in $environment_dir. Exiting."
     exit 1
 fi
 
+# The new tag
+new_tag="${RELEASE_REF}"
 
+# Update the file
+sed -i '' "s/tag:.*/tag: \"$new_tag\"/" $yaml_file
 
-
+cat $yaml_file
